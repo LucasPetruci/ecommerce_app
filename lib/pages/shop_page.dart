ffffa34411d1fbf4dcad61d8ab465d6395676eda
LucasPetruci <lucaspetruci100@gmail.com>
2024-06-17 19:19:01 -0300

    feat: shop page

---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,100 @@
+import 'package:ecommerce_app/components/shoe_tile.dart';
+import 'package:ecommerce_app/models/cart.dart';
+import 'package:ecommerce_app/models/shoe.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Cart>(
+      builder: (context, value, child) => Column(
+        children: [
+          //search bar
+          Container(
+            padding: const EdgeInsets.all(12),
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Search',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+              ],
+            ),
+          ),
+
+          //message
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25.0),
+            child: Text(
+              'everyone flies.. some fly longer than others',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+          ),
+
+          //hot picks
+
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  "Hot Picks 🔥",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+                Text(
+                  "See all",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 10), //spacing
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                //create a shoe
+                Shoe shoe = value.getShoeShop()[index];
+                return ShoeTile(
+                  shoe: shoe,
+                );
+              },
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 25, left: 25, right: 25),
+            child: Divider(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
