0935892d86c45d92d1e691be8a2a61aff7ae6dce
LucasPetruci <lucaspetruci100@gmail.com>
2024-11-18 19:26:47 -0300

    feat: add ProductDetails page to display shoe information and integrate dotenv

---
+++ lib/pages/product_details.dart
@@ -0,0 +1,32 @@
+import 'package:ecommerce_app/models/shoe.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class ProductDetails extends StatelessWidget {
+  final Shoe shoe;
+
+  const ProductDetails({super.key, required this.shoe});
+
+  @override
+  Widget build(BuildContext context) {
+    final teste = dotenv.env['TESTE'] ?? 'Valor padrão';
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(shoe.name),
+      ),
+      body: Center(
+        child: Text(
+          'Informações do sapato: ${shoe.name}\nValor do teste: $teste',
+          style: const TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
