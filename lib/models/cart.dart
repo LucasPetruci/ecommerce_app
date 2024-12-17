60bb8042933e280e160d9a595f884ca1ee29eecd
LucasPetruci <lucaspetruci100@gmail.com>
2024-12-16 22:22:37 -0300

    refactor: initialize shoeShop with constructor for better flexibility and test

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -4,42 +4,45 @@ import 'shoe.dart';
 
 class Cart extends ChangeNotifier {
   //list of shoes for sale
-  List<Shoe> shoeShop = [
-    Shoe(
-      name: 'Zoom Freak',
-      price: '236',
-      imagePath: 'lib/images/zoomfreak.png',
-      description:
-          "Domine as quadras com o Nike Zoom Freak, o tênis exclusivo do astro Giannis Antetokounmpo. Com amortecimento Zoom Air responsivo e uma estrutura leve, ele oferece suporte e estabilidade para movimentos rápidos e explosivos. Ideal para jogadores que buscam velocidade, controle e estilo.",
-    ),
-    Shoe(
-      name: 'Air Jordan',
-      price: '220',
-      imagePath: 'lib/images/AirJordan.png',
-      description:
-          "Leve o lendário estilo das quadras para as ruas com o Nike Air Jordan, o tênis que carrega o legado de Michael Jordan. Com design icônico e materiais premium, ele proporciona conforto, durabilidade e um visual incomparável. Perfeito para quem busca performance e elegância em um só modelo.",
-    ),
-    Shoe(
-      name: 'KD Trey',
-      price: '200',
-      imagePath: 'lib/images/kdtrey.png',
-      description:
-          "Inspirado no jogo versátil de Kevin Durant, o Nike KD Trey 5 combina leveza e suporte para garantir agilidade e conforto em quadra. Com amortecimento responsivo e ajuste firme, é ideal para quem procura equilíbrio entre controle e velocidade, proporcionando máximo desempenho.",
-    ),
-    Shoe(
-      name: 'Kyrie',
-      price: '190',
-      imagePath: 'lib/images/kyrie.png',
-      description:
-          "Desenvolvido para o estilo de jogo ágil e imprevisível de Kyrie Irving, o Nike Kyrie oferece tração superior e suporte direcionado para cortes rápidos e dribles desconcertantes. Seu design moderno e ajuste firme garantem conforto e performance, seja nas quadras ou nas ruas.",
-    ),
-  ];
+  List<Shoe> shoeShop;
 
-  //list of items in user cart
+  // List of items in cart
   List<Shoe> userCart = [];
 
   double deliveryPrice = 0.0;
 
+  Cart({List<Shoe>? initialShoeShop})
+      : shoeShop = initialShoeShop ??
+            [
+              Shoe(
+                name: 'Zoom Freak',
+                price: '236',
+                imagePath: 'lib/images/zoomfreak.png',
+                description:
+                    "Domine as quadras com o Nike Zoom Freak, o tênis exclusivo do astro Giannis Antetokounmpo. Com amortecimento Zoom Air responsivo e uma estrutura leve, ele oferece suporte e estabilidade para movimentos rápidos e explosivos. Ideal para jogadores que buscam velocidade, controle e estilo.",
+              ),
+              Shoe(
+                name: 'Air Jordan',
+                price: '220',
+                imagePath: 'lib/images/AirJordan.png',
+                description:
+                    "Leve o lendário estilo das quadras para as ruas com o Nike Air Jordan, o tênis que carrega o legado de Michael Jordan. Com design icônico e materiais premium, ele proporciona conforto, durabilidade e um visual incomparável. Perfeito para quem busca performance e elegância em um só modelo.",
+              ),
+              Shoe(
+                name: 'KD Trey',
+                price: '200',
+                imagePath: 'lib/images/kdtrey.png',
+                description:
+                    "Inspirado no jogo versátil de Kevin Durant, o Nike KD Trey 5 combina leveza e suporte para garantir agilidade e conforto em quadra. Com amortecimento responsivo e ajuste firme, é ideal para quem procura equilíbrio entre controle e velocidade, proporcionando máximo desempenho.",
+              ),
+              Shoe(
+                name: 'Kyrie',
+                price: '190',
+                imagePath: 'lib/images/kyrie.png',
+                description:
+                    "Desenvolvido para o estilo de jogo ágil e imprevisível de Kyrie Irving, o Nike Kyrie oferece tração superior e suporte direcionado para cortes rápidos e dribles desconcertantes. Seu design moderno e ajuste firme garantem conforto e performance, seja nas quadras ou nas ruas.",
+              ),
+            ];
   //get list of shoes for sale
   List<Shoe> getShoeShop() {
     return shoeShop;
